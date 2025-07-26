55f7500db8666bffd70318158437e7cabe845916
fanzycolly <bliss2025@qq.com>
2025-07-26 23:37:49 +0800

    init

---
+++ src/client/java/vrpg/spellbook/VRPGSpellBookClient.java
@@ -0,0 +1,10 @@
+package vrpg.spellbook;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VRPGSpellBookClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
