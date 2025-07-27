4e5ffa2bcb9ad2673fc29843dac0d28f8339d2a4
fanzycolly <bliss2025@qq.com>
2025-07-27 12:23:56 +0800

    hijack messge & add config

---
+++ src/main/java/vrpg/spellbook/Config.java
@@ -0,0 +1,50 @@
+package vrpg.spellbook;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+
+public class Config {
+    public String spellPrefix = "vrpg_spell";
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final File CONFIG_FILE = new File("vrpg/config/spell-book.json");
+
+    public static Config load() {
+        File configDir = CONFIG_FILE.getParentFile();
+        File vrpgDir = configDir.getParentFile();
+
+        if (!vrpgDir.exists() && !vrpgDir.mkdir()) {
+            VRPGSpellBook.LOGGER.error("Failed to create directory: {}", vrpgDir.getPath());
+            throw new RuntimeException("Failed to create directory: " + vrpgDir.getPath());
+        }
+
+        if (!configDir.exists() && !configDir.mkdir()) {
+            VRPGSpellBook.LOGGER.error("Failed to create directory: {}", configDir.getPath());
+            throw new RuntimeException("Failed to create directory: " + configDir.getPath());
+        }
+
+        if (!CONFIG_FILE.exists()) {
+            Config defaultConfig = new Config();
+            save(defaultConfig);
+            return defaultConfig;
+        }
+
+        try (Reader reader = new FileReader(CONFIG_FILE)) {
+            return GSON.fromJson(reader, Config.class);
+        } catch (IOException e) {
+            VRPGSpellBook.LOGGER.error("Failed to read config: {}", CONFIG_FILE.getPath(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void save(Config config) {
+        try (Writer writer = new FileWriter(CONFIG_FILE)) {
+            GSON.toJson(config, writer);
+        } catch (IOException e) {
+            VRPGSpellBook.LOGGER.error("Failed to write config: {}", CONFIG_FILE.getPath(), e);
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/vrpg/spellbook/VRPGSpellBook.java
+++ src/main/java/vrpg/spellbook/VRPGSpellBook.java
@@ -1,24 +1,28 @@
 package vrpg.spellbook;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class VRPGSpellBook implements ModInitializer {
 	public static final String MOD_ID = "vrpg-spell-book";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Config CONFIG = Config.load();
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ServerMessageEvents.ALLOW_CHAT_MESSAGE.register((message, player, param) -> {
+			var content = message.getContent().getString();
+			if (content.startsWith(CONFIG.spellPrefix)) {
+				LOGGER.info(content);
+				return false;
+			}
+			return true;
+		});
 	}
+
 }
