720b01995e4ccd0e064dc8b3c99e71fa6aca8f6b
fanzycolly <bliss2025@qq.com>
2025-07-27 19:00:33 +0800

    adding mcmti dependency

--- src/client/java/vrpg/spellbook/VRPGSpellBookClient.java
+++ src/client/java/vrpg/spellbook/VRPGSpellBookClient.java
@@ -6,5 +6,6 @@ public class VRPGSpellBookClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		//todo add mcmti mod as dependency, get language code from client and send to server
 	}
 }
--- src/main/java/vrpg/spellbook/SpellInfo.java
+++ src/main/java/vrpg/spellbook/SpellInfo.java
@@ -4,8 +4,6 @@ import java.util.Map;
 
 public class SpellInfo {
     public Map<String, String> localized;
-    //todo support chinese
-    //todo check mc's language code, use the same one
     public String action;
     public int duration;
     public int statusEffectLevel;
